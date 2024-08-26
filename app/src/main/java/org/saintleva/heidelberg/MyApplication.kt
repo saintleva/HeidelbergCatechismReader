d4737ec9ba7965c7263154a8afe2826f42878d14
Anton Liaukevich <leva.dev@gmail.com>
2024-08-26 19:53:07 +0300

    Сontinued migration from manual DI to Koin

--- app/src/main/java/org/saintleva/heidelberg/MyApplication.kt
+++ app/src/main/java/org/saintleva/heidelberg/MyApplication.kt
@@ -18,28 +18,37 @@
 package org.saintleva.heidelberg
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import org.saintleva.heidelberg.data.repository.Repository
+import org.saintleva.heidelberg.di.dataModule
+
 
 class MyApplication : Application() {
 
     lateinit var repository: Repository
 
-    init {
-        RepositoryComponent.inject(this)
-    }
-
     override fun onCreate() {
         super.onCreate()
-        repository.loadPositionFromPrefs(this)
+
+        startKoin {
+            androidLogger(Level.INFO)
+            androidContext(this@MyApplication)
+            modules(dataModule)
+        }
+
+        repository.loadPositionFromPrefs()
     }
 
     override fun onLowMemory() {
-        repository.savePositionToPrefs(this)
+        repository.savePositionToPrefs()
         super.onLowMemory()
     }
 
     override fun onTerminate() {
-        repository.savePositionToPrefs(this)
+        repository.savePositionToPrefs()
         super.onTerminate()
     }
 }
